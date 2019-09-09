551c9db55ce3069b0d453d0960da0357de00313b
CurryStep <2291158578@qq.com>
2019-09-09 09:34:41 +0800

    修改图片上传接口

--- src/test/java/com/newtechcollege/coursecms/Test.java
+++
@@ -1,7 +0,0 @@
-package com.newtechcollege.coursecms;
-public class Test{
-    public static void main(String[] args) {
-        String str = "java.lang.NumberFormatException";
-        System.out.println(str.split("\\.")[2]);
-    }
-}
